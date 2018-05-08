69d3f1cb16f0034717a7ee366c1dc68d86f90705
linkv12 <36815864+linkv12@users.noreply.github.com>
2018-05-08 22:40:21 +0700

    Update Exercise2.py

--- Exercise2.py
+++ Exercise2.py
@@ -7,7 +7,7 @@ print ("\n\n")
 if (num % 2 == 0):
   print("%d is an even number" % (num))
 elif (num % 2 != 0):
-  print("%d is an even number" % (num))
+  print("%d is an odd number" % (num))
   
   
 #END
