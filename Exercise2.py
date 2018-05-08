08a234fad9a28ea7b45ed63d01074d8c8ce9e8e2
linkv12 <36815864+linkv12@users.noreply.github.com>
2018-05-08 22:37:43 +0700

    Create Exercise2.py

---
+++ Exercise2.py
@@ -0,0 +1,13 @@
+# odd or even
+
+num = int(input("Number : "))
+
+print ("\n\n")
+
+if (num % 2 == 0):
+  print("%d is an even number" % (num))
+elif (num % 2 != ):
+  print("%d is an even number" % (num))
+  
+  
+#END
